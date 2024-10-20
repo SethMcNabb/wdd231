37dfe9fbaeb6db70133c491e37434e8f9e42d72b
SethMcNabb <sethmcnabb00@gmail.com>
2024-10-20 08:17:06 -0600

    .

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -73,4 +73,33 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchFeaturedBusiness();
 });
 
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
 
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=37.096208305119696&lon=-113.57259560267268&units=imperial&appid=671c492a0807a73bede4032a5519c1ac';
+
+async function apiFetch() {
+try {
+    const response = await fetch(url);
+    if (response.ok) {
+    const data = await response.json();
+    console.log(data);
+    } else {
+        throw Error(await response.text());
+    }
+} catch (error) {
+    console.log(error);
+}
+}
+
+apiFetch();
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp.toFixed(1)}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = `${desc}`;
+}
