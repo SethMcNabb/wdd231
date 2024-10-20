1cd22a9970278c90af1ba642a8cf08fffb516c46
SethMcNabb <sethmcnabb00@gmail.com>
2024-10-19 23:01:41 -0600

    .

--- chamber/scripts/script.js
+++ chamber/scripts/script.js
@@ -50,3 +50,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
     fetchMembers();
 });
+
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');
+const windSpeed = document.querySelector("#wind-speed");
+
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=32.70676413908385&lon=-117.15155047161586&units=imperial&appid=671c492a0807a73bede4032a5519c1ac';
+
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        //console.log(data);
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+}
+  
+
+function displayResults(weatherData) {
+    currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(1)}</strong>`;
+    windSpeed.innerHTML = `<strong>${weatherData.wind.speed.toFixed(1)}</strong>`;
+    
+    const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+    const desc = weatherData.weather[0].description;
+  
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', desc);
+    captionDesc.textContent = desc;
+}
